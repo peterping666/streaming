554d2d8340cb5826b30ac9599fa542bfa9613780
Peter <peter.ping666@gmail.com>
2020-01-01 23:49:48 -0800

    Edited version

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { signIn, signOut } from '../actions';
+import React from "react";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../actions";
 
 class GoogleAuth extends React.Component {
   componentDidMount() {
-    window.gapi.load('client:auth2', () => {
+    window.gapi.load("client:auth2", () => {
       window.gapi.client
         .init({
-          clientId:
-            '797401886567-9cumct9mrt3v2va409rasa7fa6fq02hh.apps.googleusercontent.com',
-          scope: 'email'
+          clientId: "APIkey.apps.googleusercontent.com",
+          scope: "email"
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
@@ -65,7 +64,4 @@ const mapStateToProps = state => {
   return { isSignedIn: state.auth.isSignedIn };
 };
 
-export default connect(
-  mapStateToProps,
-  { signIn, signOut }
-)(GoogleAuth);
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
